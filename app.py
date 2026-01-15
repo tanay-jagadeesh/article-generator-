8ec04d5603253ee37d08abd09913487d46bd0f86
Tanay <tanayj2107@gmail.com>
2026-01-15 17:06:27 -0500

    asking basic questions (prompts)

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -10,3 +11,14 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 
 st.title("Medium Article Generator")
 topic = st.text_input('Input your topic of interest')
+
+title_template = PromptTemplate(
+    input_variables = ['topic', 'language'],
+    template = 'Give me a medium article title on {topic} in {language} language'
+)
+
+llm = OpenAI(temperature=0.9)
+
+if topic:
+    response = llm(title_template.format(topic = topic, language = 'english'))
+    st.write(response)
